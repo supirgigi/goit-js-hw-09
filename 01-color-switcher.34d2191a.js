104988fd196a3ab444adb55169535158d9891d80
supirgigi <107944673+supirgigi@users.noreply.github.com>
2022-10-27 20:16:21 +0000

    Deploying to gh-pages from @ supirgigi/goit-js-hw-09@a3643ed4ad7ec83b6b0a906d4d7235338816ac59 🚀

---
+++ 01-color-switcher.34d2191a.js
@@ -0,0 +1,2 @@
+!function(){var t={body:document.querySelector("body"),startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")},n=null;t.stopBtn.disabled=!0,t.startBtn.addEventListener("click",(function(){n=setInterval((function(){t.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.startBtn.disabled=!0,t.stopBtn.disabled=!1})),t.stopBtn.addEventListener("click",(function(){clearInterval(n),t.startBtn.disabled=!1,t.stopBtn.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.34d2191a.js.map
